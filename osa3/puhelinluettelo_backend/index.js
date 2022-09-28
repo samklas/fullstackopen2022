56f8895ffc4eb9d8fd8c97a591467233b2171242
smabza <klassam95@gmail.com>
2022-09-28 18:10:24 +0300

    exercises 3.9-3.10

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
@@ -75,5 +76,7 @@ app.post('/api/persons', (req, res) => {
   }
 })
 
-const PORT = 3001
-app.listen(PORT, () => console.log(`Server started in ${PORT}`))
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
