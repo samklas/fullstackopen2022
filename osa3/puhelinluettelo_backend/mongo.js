5b95bce6f19351196578839c35aee236fb92ea97
smabza <klassam95@gmail.com>
2022-09-29 20:17:22 +0300

    exercise 3.12

---
+++ osa3/puhelinluettelo_backend/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstackopen:${password}@cluster0.ajxrtr7.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 4) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  
+  person.save().then(result => {
+    console.log('Person saved to db')
+    mongoose.connection.close()
+  })
+}
+
